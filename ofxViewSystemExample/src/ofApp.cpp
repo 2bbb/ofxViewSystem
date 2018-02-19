0099a55e117700bc69411c66fc22c333b1bd3e4b
ISHII 2bit <i@2bit.jp>
2018-02-20 00:11:55 +0900

    update example

--- ofxViewSystemExample/src/ofApp.cpp
+++ ofxViewSystemExample/src/ofApp.cpp
@@ -4,25 +4,48 @@
 constexpr char subview_tag[] = "subview";
 constexpr char close_button_tag[] = "close_button";
 
+struct CustomView : public bbb::vs::view {
+    CustomView(const bbb::vs::view::setting &setting)
+    : bbb::vs::view(setting)
+    {}
+    virtual void drawInternal() override {
+        const float w = getWidth(), h = getHeight(), d = (w - h) * 0.5f;
+        ofSetColor(0, 255, 0, getAlpha() * 255.0f);
+        const float t = ofGetElapsedTimef();
+        for(float x = 0.0f; x < 2.0f * M_PI; x += 0.01f) {
+            const float z = std::cos(1.3 * x + 1.1f * t) * 0.5f;
+            ofDrawLine((z * std::cos(0.3f * x + 1.3f * t) + 0.5f) * h + d,
+                       (z * std::sin(1.4f * x + 0.4f * t) + 0.5f) * h,
+                       (z * std::cos(1.9f * x + 0.7f * t) + 0.5f) * h + d,
+                       (z * std::sin(1.7f * x + 2.5f * t) + 0.5f) * h);
+        }
+    }
+    virtual void windowResizeInternal(bbb::vs::resized_event_arg arg) override {
+        auto &&v = arg.target;
+        v->setSize(arg.rect.width, arg.rect.height);
+        v->getSubview(close_button_tag)->setPosition({v->getWidth() - 10.0f, -10.0f});
+    }
+};
+
 class ofApp : public ofBaseApp {
     bbb::vs::view::ref root;
     
     void setupRootView() {
-        bbb::vs::view::setting setting(0, 0, ofGetWidth(), ofGetHeight());
-        setting.backgroundColor.set(1.0f, 0.0f, 0.0f, 0.5f);
-        setting.margin.set(20, 20, 20, 20);
+        bbb::vs::view::setting setting = bbb::vs::view::setting(0.0f, 0.0f, ofGetWidth(), ofGetHeight())
+            .setBackgroundColor(1.0f, 0.0f, 0.0f, 0.5f)
+            .setMargin(20.0f);
+        
         root = bbb::vs::view::create(setting);
         root->onClickDown([](bbb::vs::mouse_event_arg arg) {
-            auto v = arg.target;
+            auto &&v = arg.target;
             auto &&p = arg.p;
-            ofLogNotice() << "root: " << p << " / " << v->convertToLocalCoordinate(p);
-            auto &c = v->getBackgroundColor();
+            auto &&c = v->getBackgroundColor();
             c.r = 255 - c.r;
             c.b = 255 - c.b;
         });
         root->onWindowResized(bbb::vs::fitToParent);
         root->onMouseOver([](bbb::vs::mouse_event_arg arg) {
-            auto v = arg.target;
+            auto &&v = arg.target;
             if(v->isClickedNow()) {
                 v->move(v->mouseDelta());
             }
@@ -31,8 +54,9 @@ class ofApp : public ofBaseApp {
     }
     
     bbb::vs::view::ref createCloseButton() const {
-        bbb::vs::drawer::setting setting(ofRectangle(0, 0, 20, 20));
-        setting.backgroundColor.set(0.0f, 0.0f, 0.0f);
+        bbb::vs::drawer::setting setting = bbb::vs::drawer::setting(ofRectangle(0, 0, 20, 20))
+            .setBackgroundColor(0.0f, 0.0f, 0.0f);
+        
         auto &&button = bbb::vs::drawer::create(setting);
         button->onClickDown([](bbb::vs::mouse_event_arg arg) {
             auto &&v = arg.target;
@@ -54,34 +78,16 @@ class ofApp : public ofBaseApp {
     
     bbb::vs::view::ref createSubView() const {
         static const char subview_moving_animation_name[] = "subview_move";
-        bbb::vs::drawer::setting setting(ofRectangle(0, 0, root->getWidth(), root->getHeight()));
-        setting.margin = {80.0f};
-        setting.backgroundColor.set(1.0f, 1.0f, 1.0f, 0.5f);
-        setting.isEventTransparent = true;
+        bbb::vs::view::setting setting = bbb::vs::view::setting(ofRectangle(0, 0, root->getWidth(), root->getHeight()))
+            .setMargin(80.0f)
+            .setBackgroundColor(1.0f, 1.0f, 1.0f, 0.5f)
+            .setEventTransparent(true);
         
-        auto &&subview = bbb::vs::drawer::create(setting);
-        subview->onDraw([](bbb::vs::drawer::const_ref v) {
-            const float w = v->getWidth(), h = v->getHeight(), d = (w - h) * 0.5f;
-            ofSetColor(0, 255, 0, v->getAlpha() * 255.0f);
-            const float t = ofGetElapsedTimef();
-            for(float x = 0.0f; x < 2.0f * M_PI; x += 0.01f) {
-                const float z = std::cos(1.3 * x + 1.1f * t) * 0.5f;
-                ofDrawLine((z * std::cos(0.3f * x + 1.3f * t) + 0.5f) * h + d,
-                           (z * std::sin(1.4f * x + 0.4f * t) + 0.5f) * h,
-                           (z * std::cos(1.9f * x + 0.7f * t) + 0.5f) * h + d,
-                           (z * std::sin(1.7f * x + 2.5f * t) + 0.5f) * h);
-            }
-        });
+        auto &&subview = std::make_shared<CustomView>(setting);
         
         auto &&closeButton = createCloseButton();
         closeButton->setPosition({subview->getWidth() - 10.0f, -10.0f});
-        
         subview->add(close_button_tag, closeButton);
-        subview->onWindowResized([](bbb::vs::resized_event_arg arg) {
-            auto &v = arg.target;
-            v->setSize(arg.rect.width, arg.rect.height);
-            v->getSubview(close_button_tag)->setPosition({v->getWidth() - 10.0f, -10.0f});
-        });
 
         return subview;
     }
@@ -106,6 +112,7 @@ public:
                 ofToggleFullscreen();
                 break;
             case 'R':
+                root->setPosition(0, 0);
                 addSubview();
                 break;
             default:
@@ -115,6 +122,11 @@ public:
 };
 
 int main() {
-    ofSetupOpenGL(1280, 720, OF_WINDOW);
-    ofRunApp(new ofApp());
+    ofGLFWWindowSettings setting;
+    setting.width = 1280;
+    setting.height = 720;
+    auto window = ofCreateWindow(setting);
+    auto app = std::make_shared<ofApp>();
+    ofRunApp(window, app);
+    ofRunMainLoop();
 }
